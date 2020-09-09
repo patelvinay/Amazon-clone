c47b31a7cb441e4d6eddc9fc1f034b6e8dd66e02
Vinay Patel <pate0586@algonquinlive.com>
2020-09-09 13:01:55 -0700

    front page

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD0UaLRwJW-ODGLXh3XBEl7HKfh9Oh9gMc",
+    authDomain: "clone-4d092.firebaseapp.com",
+    databaseURL: "https://clone-4d092.firebaseio.com",
+    projectId: "clone-4d092",
+    storageBucket: "clone-4d092.appspot.com",
+    messagingSenderId: "497078350679",
+    appId: "1:497078350679:web:51ef51a893ea26d2b70412",
+    measurementId: "G-R88K4M4BW1"
+  };
