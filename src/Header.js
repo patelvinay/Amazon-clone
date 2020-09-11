50718044ad33e681c525dfc9f16b424108c53a41
Vinay Patel <pate0586@algonquinlive.com>
2020-09-11 07:57:25 -0700

    deploy in firebase

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,15 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function Header() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut();
+    }
+  };
 
   return (
     <div className="header">
@@ -23,10 +29,14 @@ function Header() {
       </div>
 
       <div className="header__nav">
-        <div className="header__option">
-          <span className="header__optionLineOne">Hello Guest</span>
-          <span className="header__optionLineTwo">Sign in</span>
-        </div>
+        <Link to={!user && "/login"}>
+          <div onClick={handleAuthentication} className="header__option">
+            <span className="header__optionLineOne">Hello Guest</span>
+            <span className="header__optionLineTwo">
+              {user ? "Sign Out" : "Sign In"}
+            </span>
+          </div>
+        </Link>
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
           <span className="header__optionLineTwo">& Orders</span>
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,19 @@
+import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD0UaLRwJW-ODGLXh3XBEl7HKfh9Oh9gMc",
-    authDomain: "clone-4d092.firebaseapp.com",
-    databaseURL: "https://clone-4d092.firebaseio.com",
-    projectId: "clone-4d092",
-    storageBucket: "clone-4d092.appspot.com",
-    messagingSenderId: "497078350679",
-    appId: "1:497078350679:web:51ef51a893ea26d2b70412",
-    measurementId: "G-R88K4M4BW1"
-  };
+  apiKey: "AIzaSyD0UaLRwJW-ODGLXh3XBEl7HKfh9Oh9gMc",
+  authDomain: "clone-4d092.firebaseapp.com",
+  databaseURL: "https://clone-4d092.firebaseio.com",
+  projectId: "clone-4d092",
+  storageBucket: "clone-4d092.appspot.com",
+  messagingSenderId: "497078350679",
+  appId: "1:497078350679:web:51ef51a893ea26d2b70412",
+  measurementId: "G-R88K4M4BW1",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
