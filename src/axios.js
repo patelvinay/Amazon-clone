2eef9fa3eb9120423cc4c94d68b8db2da0940aa1
Vinay Patel <pate0586@algonquinlive.com>
2020-09-13 07:45:31 -0700

    deploying with functions

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-4d092/us-central1/api", //the api  (cloud function) url
+  baseURL: "https://us-central1-clone-4d092.cloudfunctions.net/api", //the api  (cloud function) url
+  // http://localhost:5001/clone-4d092/us-central1/api
 });
 
 export default instance;
